83d829ffd11e691fe65aa8b0bf4fe2bc19024b3e
Artem Rakov <scoff1k@gmail.com>
2017-11-26 22:01:01 +0000

    Added home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,20 @@
 <template>
-  <h1>The home components</h1>
+  <div>
+    <h1>Trade view your Portfolio</h1>
+    <h6>Your may Save & Load your Data</h6>
+    <h6>Click on 'End Day' to begin a new day!</h6>
+    <hr>
+    <p> Your funds: {{funds}} </p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    funds() {
+      return this.$store.getters.funds
+    }
+  }
 }
 </script>
 
